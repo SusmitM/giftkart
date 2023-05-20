77979b4da074d09da8803acc8589c938e8cdead9
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2023-05-21 00:45:43 +0530

    feat: products page link added in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export const Home = () => {
+  const navigate=useNavigate();
   return (
     <>
     <div>Home</div>
+    <button onClick={()=>navigate('/products')}>Products</button>
     </>
   )
 }
