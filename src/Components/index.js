cc1a1a158040b8d2c253aaabfd92fc198554f0da
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2023-05-21 18:53:12 +0530

    import src changed

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export {Navigation} from "./Navigation/Navigation";
 export {Filters} from "./Filters/Filters";
 export {Loader} from "./Loader/Loader";
+export {SingleProductCard} from "./SingleProductCard/SingleProductCard";
