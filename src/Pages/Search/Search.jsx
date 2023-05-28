85f18c413ed4fd590ae45f8ed5cbc83897c5fc67
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2023-05-28 14:54:34 +0530

    feat: search page added

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,8 +1,43 @@
-
-import React from 'react'
+import { useState } from "react";
+import { useDataContext } from "../../context/data/dataContext";
+import { ProductCard } from "../../Components/ProductCard/ProductCard";
+import { BsSearch } from "react-icons/bs";
+import "./Search.css";
 
 export const Search = () => {
+  const { productsData } = useDataContext();
+  const [searchData, setSearchData] = useState("");
+  const searchFilteredData=()=>{
+    if(searchData===""){
+      return productsData
+    }
+    else{
+      return productsData.filter(item=>item.name.toLowerCase().includes(searchData.toLowerCase()) || item.product.toLowerCase().includes(searchData.toLowerCase()))
+    }
+  }
   return (
-    <div>Search</div>
-  )
-}
+    <>
+      <div className="search-page-container">
+        <div className="search-container">
+          <div className="searchBar">
+            <BsSearch />
+            <input
+              type="text"
+              name="searchBar"
+              placeholder="Enter the name or type of Product"
+              value={searchData}
+              onChange={(e) => setSearchData(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="product-display-container">
+          <ul className="product-list">
+            {searchFilteredData().map((product) => {
+              return <ProductCard key={product._id} productData={product} />;
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
